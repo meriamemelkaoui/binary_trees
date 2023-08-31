c82e19208173332df237ceb4da31f3dac1682f17
Meriame Melkaoui <meriamemelkaoui2003@gmail.com>
2023-08-31 14:23:31 -0700

    commit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,26 +7,26 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_left;
+	binary_tree_t *new_left;
 
-    if (parent == NULL)
-    {
-        return (NULL);
-    }
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 
-    new_left = binary_tree_node(parent, value);
-    if (!new_left)
-    {
-        return (NULL);
-    }
+	new_left = binary_tree_node(parent, value);
+	if (!new_left)
+	{
+		return (NULL);
+	}
 
-    if (parent->left)
-    {
-        new_left->left = parent->left;
-        parent->left->parent = new_left;
-    }
+	if (parent->left)
+	{
+		new_left->left = parent->left;
+		parent->left->parent = new_left;
+	}
 
-    parent->left = new_left;
+	parent->left = new_left;
 
-    return (new_left);
+	return (new_left);
 }
